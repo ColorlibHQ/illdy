49df24940d1c1f62a458556fd0451c1fbe5a6c97
Giucu <giucu91@yahoo.com>
2016-12-22 04:02:40 +0200

    #32

--- functions.php
+++ functions.php
@@ -33,7 +33,6 @@ if ( ! function_exists( 'illdy_setup' ) ) {
 		add_theme_support( 'automatic-feed-links' );
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'custom-background' );
 		add_theme_support( 'custom-logo', array(
 			'height'      => 75,
    			'width'       => 105,
