543bb4b916be1dc9e06bee5466ec1fcb8fff7389
Giucu <giucu91@yahoo.com>
2017-09-13 11:01:41 +0300

    Fix typo in demo content #219

--- sections/front-page-about.php
+++ sections/front-page-about.php
@@ -9,7 +9,7 @@
 <?php
 if ( current_user_can( 'edit_theme_options' ) ) {
 	$general_title = get_theme_mod( 'illdy_about_general_title', __( 'About', 'illdy' ) );
-	$general_entry = get_theme_mod( 'illdy_about_general_entry', __( 'It is an amazng one-page theme with great features that offers an incredible experience. It is easy to install, make changes, adapt for your business. A modern design with clean lines and styling for a wide variety of content, exactly how a business design should be. You can add as many images as you want to the main header area and turn them into slider.', 'illdy' ) );
+	$general_entry = get_theme_mod( 'illdy_about_general_entry', __( 'It is an amazing one-page theme with great features that offers an incredible experience. It is easy to install, make changes, adapt for your business. A modern design with clean lines and styling for a wide variety of content, exactly how a business design should be. You can add as many images as you want to the main header area and turn them into slider.', 'illdy' ) );
 } else {
 	$general_title = get_theme_mod( 'illdy_about_general_title' );
 	$general_entry = get_theme_mod( 'illdy_about_general_entry' );
