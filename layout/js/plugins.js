1f989bc0636cf2ef5e05e02f9a866dd2046bd141
Giucu <giucu91@yahoo.com>
2017-12-19 13:54:24 +0200

    Travis Review

--- layout/js/plugins.js
+++ layout/js/plugins.js
@@ -1,87 +1,104 @@
-/**
- *	jQuery Document Ready
- */
-jQuery( document ).ready( function($) {
-	// Progress Bar
-	function illdyProgressBar() {
-		if( $( '#about .skill' ).length ) {
-				$( '#about .skill' ).each( function() {
-				var skill = $( this );
-				var skillProgressBar = $( skill ).children( '.skill-top' ).children( '.skill-progress-bar' );
-				var dataSkillProgressBarWidth = $( skill ).data( 'skill-progress-bar-width' );
-				var dataSkillColor = $( skill ).data( 'skill-color' );
-				var skillBottom = $( skill ).children( '.skill-bottom' );
 
-				$( skillProgressBar ).progressbar({
-					value: dataSkillProgressBarWidth
-				});
+jQuery( document ).ready( function( $ ) {
 
-				$( this ).children( '.skill-top' ).children( '.skill-progress-bar' ).children( '.ui-progressbar-value' ).css( 'background-color', dataSkillColor );
-				$( this ).children( '.skill-top' ).children( '.skill-progress-bar' ).children( '.ui-progressbar-value' ).append( '<span class="ui-progressbar-value-circle" style="background-color: '+ dataSkillColor +'"></span>' );
-				$( this ).children( '.skill-top' ).children( '.skill-progress-bar' ).children( '.ui-progressbar-value' ).append( '<span class="ui-progressbar-value-top" style="background-color: '+ dataSkillColor +'"></span>' );
-				$( this ).children( '.skill-top' ).children( '.skill-progress-bar' ).children( '.ui-progressbar-value' ).children( '.ui-progressbar-value-top' ).text( dataSkillProgressBarWidth + '%' );
-				$( this ).children( '.skill-top' ).children( '.skill-progress-bar' ).children( '.ui-progressbar-value' ).children( '.ui-progressbar-value-top' ).append( '<span class="ui-progressbar-value-triangle" style="border-top-color: '+ dataSkillColor +'; border-right-color: transparent; border-bottom-color: transparent; border-left-color: transparent;"></span>' );
-				$( this ).children( '.skill-bottom' ).css( 'color', dataSkillColor );
-			});
-		}
-	}
+  // Progress Bar
+  function illdyProgressBar() {
+    if ( $( '#about .skill' ).length ) {
+      $( '#about .skill' ).each( function() {
+        var skill = $( this );
+        var skillProgressBar = $( skill ).children( '.skill-top' ).children( '.skill-progress-bar' );
+        var dataSkillProgressBarWidth = $( skill ).data( 'skill-progress-bar-width' );
+        var dataSkillColor = $( skill ).data( 'skill-color' );
 
-	// Testimonials OWL Carousel
-	function testimonialsOwlCarousel() {
-		if( $( '.testimonials-carousel.owl-carousel-enabled .widget_illdy_testimonial' ).length > 1 ) {
-			$( '.testimonials-carousel.owl-carousel-enabled' ).owlCarousel({
-				'items': 1,
-				'loop': true,
-				'dots': true
-			});
-		}
-	}
+        $( skillProgressBar ).progressbar( {
+          value: dataSkillProgressBarWidth
+        } );
 
-	// Counter Number
-	function counterNumber() {
-		if( $( '#counter .counter-number' ).length ) {
-			$( '#counter .counter-number' ).countTo();
-		}
-	}
+        $( this ).children( '.skill-top' ).children( '.skill-progress-bar' ).children( '.ui-progressbar-value' ).css( 'background-color', dataSkillColor );
+        $( this ).
+            children( '.skill-top' ).
+            children( '.skill-progress-bar' ).
+            children( '.ui-progressbar-value' ).
+            append( '<span class="ui-progressbar-value-circle" style="background-color: ' + dataSkillColor + '"></span>' );
+        $( this ).
+            children( '.skill-top' ).
+            children( '.skill-progress-bar' ).
+            children( '.ui-progressbar-value' ).
+            append( '<span class="ui-progressbar-value-top" style="background-color: ' + dataSkillColor + '"></span>' );
+        $( this ).
+            children( '.skill-top' ).
+            children( '.skill-progress-bar' ).
+            children( '.ui-progressbar-value' ).
+            children( '.ui-progressbar-value-top' ).
+            text( dataSkillProgressBarWidth + '%' );
+        $( this ).
+            children( '.skill-top' ).
+            children( '.skill-progress-bar' ).
+            children( '.ui-progressbar-value' ).
+            children( '.ui-progressbar-value-top' ).
+            append( '<span class="ui-progressbar-value-triangle" style="border-top-color: ' + dataSkillColor +
+                '; border-right-color: transparent; border-bottom-color: transparent; border-left-color: transparent;"></span>' );
+        $( this ).children( '.skill-bottom' ).css( 'color', dataSkillColor );
+      } );
+    }
+  }
 
-	// Front Page jumbotron Slider
-	function illdyJumbotronSlider(){
-		var illdySlider;
-		if ( jQuery( '.illdy-slider' ).length > 0 ) {
-			illdySlider = jQuery( '.illdy-slider' );
-			illdySlider.owlCarousel({
-				'items': 1,
-				'loop': true,
-				'dots': false,
-				'autoplay' : illdySlider.data( 'autoplay' ),
-				'autoplayTimeout' : illdySlider.data( 'autoplay-time' ),
-			});
-			if ( jQuery( '.illdy-slider-navigation' ).length > 0 ) {
-				jQuery( '.illdy-slider-navigation #prev' ).click( function(evt){
-					evt.preventDefault();
-					illdySlider.trigger('prev.owl.carousel');
-				});
-				jQuery( '.illdy-slider-navigation #next' ).click( function(evt){
-					evt.preventDefault();
-					illdySlider.trigger('next.owl.carousel');
-				});
-			}
-		}
-		
-	}
+  // Testimonials OWL Carousel
+  function testimonialsOwlCarousel() {
+    if ( $( '.testimonials-carousel.owl-carousel-enabled .widget_illdy_testimonial' ).length > 1 ) {
+      $( '.testimonials-carousel.owl-carousel-enabled' ).owlCarousel( {
+        'items': 1,
+        'loop': true,
+        'dots': true
+      } );
+    }
+  }
 
-	// Called Functions
-	$( function() {
-		illdyProgressBar();
-		testimonialsOwlCarousel();
-		illdyJumbotronSlider();
+  // Counter Number
+  function counterNumber() {
+    var counter = $( '#counter' ).find( '.counter-number' );
+    if ( counter.length ) {
+      counter.countTo();
+    }
+  }
 
-		$( window ).scroll( function() {
-			var counterVisible = $( '#counter' ).visible();
+  // Front Page jumbotron Slider
+  function illdyJumbotronSlider() {
+    var illdySlider = jQuery( '.illdy-slider' );
+    if ( illdySlider.length > 0 ) {
+      illdySlider.owlCarousel( {
+        'items': 1,
+        'loop': true,
+        'dots': false,
+        'autoplay': illdySlider.data( 'autoplay' ),
+        'autoplayTimeout': illdySlider.data( 'autoplay-time' )
+      } );
+      if ( jQuery( '.illdy-slider-navigation' ).length > 0 ) {
+        jQuery( '.illdy-slider-navigation #prev' ).click( function( evt ) {
+          evt.preventDefault();
+          illdySlider.trigger( 'prev.owl.carousel' );
+        } );
+        jQuery( '.illdy-slider-navigation #next' ).click( function( evt ) {
+          evt.preventDefault();
+          illdySlider.trigger( 'next.owl.carousel' );
+        } );
+      }
+    }
 
-			if( counterVisible == true ) {
-				counterNumber();
-			}
-		});
-	});
-});
+  }
+
+  // Called Functions
+  $( function() {
+    illdyProgressBar();
+    testimonialsOwlCarousel();
+    illdyJumbotronSlider();
+
+    $( window ).scroll( function() {
+      var counterVisible = $( '#counter' ).visible();
+
+      if ( true === counterVisible ) {
+        counterNumber();
+      }
+    } );
+  } );
+} );
