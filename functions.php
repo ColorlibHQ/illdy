a58f0609e9d73cb74270790d6d24b5df6b9f0dd9
Cristian Raiber <cristian@machothemes.com>
2018-05-09 11:35:28 +0300

    fixed wrong url for loading fancybox JS

--- functions.php
+++ functions.php
@@ -200,7 +200,7 @@ if ( ! function_exists( 'illdy_enqueue_javascripts' ) ) {
 		wp_enqueue_script( 'illdy-count-to', get_template_directory_uri() . '/layout/js/count-to/count-to.min.js', array( 'jquery' ), '', true );
 		wp_enqueue_script( 'illdy-visible', get_template_directory_uri() . '/layout/js/visible/visible.min.js', array( 'jquery' ), '', true );
 		if ( get_theme_mod( 'illdy_projects_lightbox', 0 ) == 1 ) {
-			wp_enqueue_script( 'illdy-fancybox', get_template_directory_uri() . '/layout/js/fancybox/jquery.fancybox.min.js', array( 'jquery' ), '3.3.5', true );
+			wp_enqueue_script( 'illdy-fancybox', get_template_directory_uri() . '/layout/js/fancybox/jquery-fancybox.min.js', array( 'jquery' ), '3.3.5', true );
 			wp_add_inline_script( 'illdy-fancybox', 'jQuery(".fancybox").fancybox();' );
 		}
 		if ( get_theme_mod( 'illdy_sticky_header_enable', false ) ) {
